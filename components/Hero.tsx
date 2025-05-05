75cb76185c7e3cf5cb9c99c4f76aaaa8621ca931
Tim Benniks <tbenniks@gmail.com>
2025-05-05 13:03:14 +0200

    refactor: restructure Hero component to improve layout and integrate Image component

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -1,4 +1,5 @@
-export interface HeroProps {
+import Image from "next/image";
+interface HeroProps {
   $?: any;
   _metadata: { $?: any; uid: string };
   description: string;
@@ -21,9 +22,26 @@ export default function Hero({
   video,
   $,
 }: HeroProps) {
+  console.log(design);
+
   return (
-    <>
-      <h1 {...($ && $.title)}>{title}</h1>
-    </>
+    <div className="aspect-[1440/635] relative w-full overflow-hidden bg-gray-100">
+      <Image
+        {...($ && $.image)}
+        src={image?.url}
+        alt={title}
+        width={1440}
+        height={635}
+        loading="eager"
+        fetchPriority="high"
+        quality={100}
+        priority={true}
+        className="h-full w-full object-cover object-center"
+      />
+      <article className="max-w-[400px] left-16 md:absolute top-2/4 md:-translate-y-2/4 mt-4 md:mt-0 px-4 md:px-0 text-center">
+        {title ? <h1 {...($ && $.title)}>{title}</h1> : null}
+        {description ? <h1 {...($ && $.description)}>{description}</h1> : null}
+      </article>
+    </div>
   );
 }
