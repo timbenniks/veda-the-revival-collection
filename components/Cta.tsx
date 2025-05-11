a081795b8f0601f4376536da70dc563c3668f7b7
Tim Benniks <tbenniks@gmail.com>
2025-05-11 18:50:56 +0200

    refactor: enhance Cta component with improved Link styling and formatting

--- components/Cta.tsx
+++ components/Cta.tsx
@@ -8,7 +8,12 @@ interface CtaProps {
 
 export default function Cta({ link, text, $ }: CtaProps) {
   return (
-    <Link {...($ && $.link)} href={link?.href} title={link?.title}>
+    <Link
+      {...($ && $.link)}
+      href={link?.href}
+      title={link?.title}
+      className="block px-2 py-1 border-b border-black no-underline uppercase hover:bg-black hover:text-white"
+    >
       {$ ? <span {...($ && $.text)}>{text}</span> : text}
     </Link>
   );
